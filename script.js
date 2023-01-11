29057639e139194bb1efa43f14e76158d2efa9ad
ilyasse <ilyasse.ettamri@gmail.com>
2023-01-11 14:46:44 +0100

    project start

---
+++ script.js
@@ -0,0 +1,17 @@
+// Page d'acceuil => barre de recherche (tri automatique par nom de recette)
+// Page d'acceuil => 6 cartes de recettes affichées aléatoirement
+// Deuxième page contient deux listes déroulantes (Catégories - Région)
+// La catégorie (Lamb) et la région (Moroccan) sont séléctionnés par défaut
+// Pagination avec 6 recettes par page maximum
+// La carte de recette doit contenir son nom, image et un boutton qui affiche plus de details en une fenêtre modale
+// fenêtre modale affiche le nom, l'image, la catégorie, la région, la liste des ingrédients et les instructions de préparation
+
+{/* <div class="card" style="width: 30%;">
+<img src="..." class="card-img-top" alt="...">
+<ul class="list-group list-group-flush">
+<li class="list-group-item">A third item</li>
+</ul>
+<div class="card-body">
+<a href="#" class="btn btn-primary">Go somewhere</a>
+</div>
+</div> */}
