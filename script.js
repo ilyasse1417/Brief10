997621f81cd20ad6da449bbb489dd179d72837a5
ilyasse <ilyasse.ettamri@gmail.com>
2023-01-30 15:26:10 +0100

    Sans pagination

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
-// Page d'acceuil => barre de recherche (tri automatique par nom de recette)
-// Page d'acceuil => 6 cartes de recettes affichées aléatoirement
-// Deuxième page contient deux listes déroulantes (Catégories - Région)
-// La catégorie (Lamb) et la région (Moroccan) sont séléctionnés par défaut
-// Pagination avec 6 recettes par page maximum
-// La carte de recette doit contenir son nom, image et un boutton qui affiche plus de details en une fenêtre modale
-/* fenêtre modale affiche le nom, l'image, la catégorie, la région, la liste des ingrédients et les instructions de préparation */
-  
 var randMeal
 var mealArray = []
-
-
 for(i=0 ; i < 6 ; i++){
 $.ajax({
     url:'https://www.themealdb.com/api/json/v1/1/random.php',
@@ -21,6 +11,23 @@ $.ajax({
     }
 })
 }
+jQuery.ajax({ 
+    url: 'https://themealdb.com/api/json/v1/1/list.php?c=list', 
+    type:'GET',
+    async: false,
+    success:function(data){
+        selectBox('#category',data.meals,'strCategory')
+    }
+    });
+jQuery.ajax({ 
+    url: 'https://themealdb.com/api/json/v1/1/list.php?a=list', 
+    type:'GET',
+    async: false,
+    success:function(data){
+        selectBox('#region',data.meals,'strArea')
+    }
+    });
+    
 cardInsert(mealArray)
 
 function cardInsert(data){
@@ -39,6 +46,22 @@ function cardInsert(data){
         cards.innerHTML += addCard
 }
 }
+function cardInsert2(data){
+    let cards2 = document.getElementById('cards2')
+    cards2.innerHTML = ''
+    let addCard
+    for(i=0; i < data.length; i++){
+        addCard = `<div class="card" style="width: 25%;">
+        <img src="${data[i].strMealThumb}" class="card-img-top" alt="...">
+        <ul class="list-group list-group-flush">
+        <li class="list-group-item">${data[i].strMeal}</li>
+        </ul>
+        <div class="card-body">
+        <a href="#" onclick="showModal(${data[i].idMeal})" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#dataModal">Plus de détails</a>
+        </div>
+        </div> `
+        cards2.innerHTML += addCard
+}}
 function showModal(id){
     let detailArray = []
     $.ajax({
@@ -69,4 +92,123 @@ function showModal(id){
     document.getElementById('detailIngredients').innerHTML = ingredients
     document.getElementById('detailMeasures').innerHTML = measures
 
+}
+function searchMeal(inputData){
+    var mealFound = [];
+    jQuery.ajax({ 
+        url: 'https://themealdb.com/api/json/v1/1/search.php?s='+inputData, 
+        type:'GET',
+        async: false,
+        success:function(data){
+    
+            mealFound = data.meals;
+
+        }
+    });
+    return mealFound ;
+}
+function searchGo(){
+    let searchText = document.querySelector('#searchInp').value
+    let cards = document.getElementById('cards')
+    cards.innerHTML = ''
+    const mealFound = searchMeal(searchText)
+    cardInsert(mealFound)
+}
+function selectBox(id, array, boxType){
+    selectBoxID = document.querySelector(`${id}`)
+    for (let i = 0; i < array.length; i++) {
+        if(array[i][`${boxType}`] === 'Moroccan')
+        selectBoxID.innerHTML += `<option value="${array[i][`${boxType}`]}" selected>${array[i][`${boxType}`]}</option>`
+        else if(array[i][`${boxType}`] === 'Beef')
+        selectBoxID.innerHTML += `<option value="${array[i][`${boxType}`]}" selected>${array[i][`${boxType}`]}</option>`
+        else
+        selectBoxID.innerHTML += `<option value="${array[i][`${boxType}`]}">${array[i][`${boxType}`]}</option>`
+    }
+}
+function sortBy(){
+    document.querySelector('#notFound').innerHTML = ''
+    category = document.querySelector('#category').value
+    region = document.querySelector('#region').value
+    let categoryList = []
+    let regionList = []
+    let filteredList = []
+    jQuery.ajax({ 
+        url: `https://themealdb.com/api/json/v1/1/filter.php?c=${category}`, 
+        type:'GET',
+        async: false,
+        success:function(data){
+            categoryList = data.meals
+        }
+    });
+    if(category === 'All' && region === 'All'){
+        let fullCatList = []
+        jQuery.ajax({ 
+            url: `https://themealdb.com/api/json/v1/1/list.php?c=list`, 
+            type:'GET',
+            async: false,
+            success:function(data){
+                fullCatList = data.meals
+                console.log(fullCatList)
+            }
+        });
+        let fullMeals = []
+        for(i=0 ; i<fullCatList.length;i++){
+            jQuery.ajax({ 
+                url: `https://themealdb.com/api/json/v1/1/filter.php?c=${fullCatList[i].strCategory}`, 
+                type:'GET',
+                async: false,
+                success:function(data){
+                    fullMeals.push(data.meals)
+                }
+            });
+        }
+        cardInsert2(fullMeals.flat(1))
+    }
+    if(category !== 'All' && region === 'All'){
+        jQuery.ajax({ 
+            url: `https://themealdb.com/api/json/v1/1/filter.php?c=${category}`, 
+            type:'GET',
+            async: false,
+            success:function(data){
+                categoryList = data.meals
+            }
+        });
+        let regionAll = []
+        for (let i = 0; i < categoryList.length; i++) {
+            regionAll.push(categoryList[i])
+        }
+        cardInsert2(regionAll)
+    }
+    if(category === 'All' && region !== 'All'){
+        jQuery.ajax({ 
+            url: `https://themealdb.com/api/json/v1/1/filter.php?a=${region}`, 
+            type:'GET',
+            async: false,
+            success:function(data){
+                regionList = data.meals
+            }
+        });
+        let categoryAll = []
+        for (let i = 0; i < regionList.length; i++) {
+            categoryAll.push(regionList[i])
+        }
+        cardInsert2(categoryAll)
+    }
+    jQuery.ajax({ 
+        url: `https://themealdb.com/api/json/v1/1/filter.php?a=${region}`, 
+        type:'GET',
+        async: false,
+        success:function(data){
+            regionList = data.meals
+        }
+    });
+    for (let i = 0; i < categoryList.length; i++) {
+        for (let j = 0; j < regionList.length; j++) {
+            if(+regionList[j].idMeal == +categoryList[i].idMeal)
+                filteredList.push(regionList[j])
+        }
+    }
+    cardInsert2(filteredList)
+    if(filteredList.length === 0)
+    document.querySelector('#notFound').innerHTML = 'Meal not found!'
 }
